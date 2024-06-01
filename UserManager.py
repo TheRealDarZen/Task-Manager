dd02989a79ddd27e3133294e5fc29e430feaa894
darzen <nivnik004@gmail.com>
2024-06-01 20:28:18 +0200

    Added UserManager class.

---
+++ UserManager.py
@@ -0,0 +1,23 @@
+import hashlib
+import shelve
+
+
+class UserManager:
+    def __init__(self, db_name='database/users.db'):
+        self.db_name = db_name
+
+    def hash_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    def create_user(self, username, password):
+        with shelve.open(self.db_name) as db:
+            if username in db:
+                return False
+            db[username] = self.hash_password(password)
+        return True
+
+    def validate_user(self, username, password):
+        with shelve.open(self.db_name) as db:
+            if username in db and db[username] == self.hash_password(password):
+                return True
+        return False
